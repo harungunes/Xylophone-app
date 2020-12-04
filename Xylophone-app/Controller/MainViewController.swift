bce7dbfe8b5b2562e4eac21d8e8df39e612bf92c
Harun Gunes <h.gunes13@gmail.com>
2020-12-05 00:12:17 +0100

    Initialize repo

---
+++ Xylophone-app/Controller/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Xylophone-app
+//
+//  Created by Harun Gunes on 05/12/2020.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .yellow
+  }
+}
+
