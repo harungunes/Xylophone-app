4bcccb8673a48d9411ca29afcbdac7387f342d5b
Harun Gunes <h.gunes13@gmail.com>
2020-12-05 03:05:33 +0100

    Updated the button items

--- Xylophone-app/View/AButton.swift
+++ Xylophone-app/View/AButton.swift
@@ -5,4 +5,40 @@
 //  Created by Harun Gunes on 05/12/2020.
 //
 
-import Foundation
+import UIKit
+
+class AButton {
+  let buttonA: UIButton = {
+    let button = UIButton(type: .system)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("A", for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 32)
+    button.setTitleColor(.white, for: .normal)
+    button.backgroundColor = .blue
+    
+    return button
+  }()
+  
+  func activateButtonA() {
+    buttonA.addTarget(self, action: #selector(playA), for: .touchUpInside)
+  }
+  
+  @objc func playA() {
+    print("TEST A")
+  }
+  
+  func setButtonA() {
+    
+    activateButtonA()
+    viewA.addSubview(buttonA)
+    
+    NSLayoutConstraint.activate([
+      
+      buttonA.centerXAnchor.constraint(equalTo: viewA.centerXAnchor),
+      buttonA.centerYAnchor.constraint(equalTo: viewA.centerYAnchor),
+      buttonA.widthAnchor.constraint(equalTo: viewA.widthAnchor, multiplier: 0.85),
+      buttonA.heightAnchor.constraint(equalTo: viewA.heightAnchor, multiplier: 0.9)
+    ])
+  }
+  
+}
--- Xylophone-app/View/EButton.swift
+++ Xylophone-app/View/EButton.swift
@@ -5,4 +5,41 @@
 //  Created by Harun Gunes on 05/12/2020.
 //
 
-import Foundation
+import UIKit
+
+class EButton {
+  let buttonE: UIButton = {
+    let button = UIButton(type: .system)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("E", for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 32)
+    button.setTitleColor(.white, for: .normal)
+    button.backgroundColor = .systemTeal
+    
+    return button
+  }()
+  
+  func activateButtonE() {
+    buttonE.addTarget(self, action: #selector(playE), for: .touchUpInside)
+  }
+  
+  @objc func playE() {
+    print("TEST E")
+  }
+  
+  func setButtonE() {
+    
+    activateButtonE()
+    viewE.addSubview(buttonE)
+    
+    NSLayoutConstraint.activate([
+      
+      buttonE.centerXAnchor.constraint(equalTo: viewE.centerXAnchor),
+      buttonE.centerYAnchor.constraint(equalTo: viewE.centerYAnchor),
+      buttonE.widthAnchor.constraint(equalTo: viewE.widthAnchor, multiplier: 0.94),
+      buttonE.heightAnchor.constraint(equalTo: viewE.heightAnchor, multiplier: 0.99)
+    ])
+  }
+  
+}
+
